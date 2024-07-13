1f6be11ca88e0bd45b38f0f6a0a6fbe5b3e11715
Yevhenii Dubrovin <110616229+YevheniiDubrovin@users.noreply.github.com>
2024-07-14 00:06:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     })
 })
+
+// const faqs = document.querySelectorAll(".faq");
+
+// faqs.forEach(faq => {
+//     faq.addEventListener("click", () => {
+//         faqs.forEach(item => {
+//             if (item !== faq) {
+//                 item.classList.remove("active");
+//             }
+//         });
+//         faq.classList.toggle("active");
+//     });
+// });
